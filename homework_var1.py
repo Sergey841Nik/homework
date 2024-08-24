bab30ecc5769165bbefefcf935df32a0f63fe418
Sergey841Nik <nikitos841@yandex.ru>
2024-08-24 20:25:17 +0400

    Add files via upload

---
+++ homework_var1.py
@@ -0,0 +1,8 @@
+make_hw = 12
+hours_spent = 1.5
+course_name = "Python"
+hours_one_hw = hours_spent / make_hw
+
+print(
+    f"Курс: {course_name}, всего задач: {make_hw}, затрачено часов: {hours_spent}, среднее время выполнения {hours_one_hw} часа."
+)
