6ce69d5e575fe31a11c30382bc043e2ae94f88f0
Сергей <nikitos841@yandex.ru>
2024-09-02 19:29:27 +0400

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+len_my_list = len(my_list)
+n = 0
+
+while n < len_my_list:
+    if my_list[n] > 0:
+        print(my_list[n])
+    elif my_list[n] < 0:
+        break
+    n += 1
