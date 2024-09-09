299865016a4dec7cc6dace4cba41edd9c8407834
Сергей <nikitos841@yandex.ru>
2024-09-09 19:15:51 +0400

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,6 +1,10 @@
-
-def send_email(message, recipient, *, sender = "university.help@gmail.com"):
-    if not '@' in recipient or not '@' in sender and sender.endswith(('.com', '.net', '.ru')) or not recipient.endswith(('.com', '.net', '.ru')):
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    if (
+        not "@" in recipient
+        or not "@" in sender
+        and sender.endswith((".com", ".net", ".ru"))
+        or not recipient.endswith((".com", ".net", ".ru"))
+    ):
         return f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}"
     elif recipient == sender:
         return "Нельзя отправить письмо самому себе!"
@@ -8,8 +12,9 @@ def send_email(message, recipient, *, sender = "university.help@gmail.com"):
         return f"Письмо {message} успешно отправлено с адреса {sender} на адрес {recipient}."
     else:
         return f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо {message} отправлено с адреса {sender} на адрес {recipient}."
-    
+
+
 print(send_email("Привет!", "university.help@gmail.com"))
 print(send_email("Привет!", "skuka@yandex.ru"))
-print(send_email("Привет!", "skuka@yandex.ru", sender = "university.help@yandex.ru"))
-print(send_email("Привет!", "skukayandex.ru", sender = "university.help@yandex.ru"))
+print(send_email("Привет!", "skuka@yandex.ru", sender="university.help@yandex.ru"))
+print(send_email("Привет!", "skukayandex.ru", sender="university.help@yandex.ru"))
