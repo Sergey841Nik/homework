5c66366a4d9c2b7f69617d705ca046dc6c6c4853
Сергей <nikitos841@yandex.ru>
2024-10-02 19:50:51 +0400

    add module_5_3

---
+++ module_5_3.py
@@ -0,0 +1,92 @@
+
+class House:
+    def __init__(self, name: str, number_of_floors: int):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        self.valid(number_of_floors)
+
+    def valid(self, values: int):
+        if not (
+            isinstance(self.name, str) and
+            isinstance(self.number_of_floors, int) and
+            isinstance(values, int) or
+            isinstance(values, House)
+
+        ):
+            raise TypeError("Неверный фоомат(type) введённых данных")
+
+    def __str__(self):
+        return f"Название дома: {self.name}, количество этажей: {self.number_of_floors}"
+    
+    def __len__(self):
+        return self.number_of_floors
+    
+    def __add__(self, other):
+        self.valid(other)
+        self.number_of_floors += other
+        return self
+    
+    def __radd__(self, other):
+        self.valid(other)
+        return self.__add__(other)
+    
+    def __iadd__(self, other):
+        self.valid(other)
+        return self.__add__(other)
+    
+    def __eq__(self, other):
+        self.valid(other)
+        return self.number_of_floors == other
+    
+    def __ne__(self, other):
+        self.valid(other)
+        return self.number_of_floors != other
+    
+    def __gt__(self, other):
+        self.valid(other)
+        return self.number_of_floors > other
+    
+    def __ge__(self, other):
+        self.valid(other)
+        return self.number_of_floors >= other
+    
+    def __lt__(self, other):
+        self.valid(other)
+        return self.number_of_floors < other
+    
+    def __le__(self, other):
+        self.valid(other)
+        return self.number_of_floors <= other
+
+    def go_to(self, new_floor: int):
+        self.valid(new_floor)
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor+1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+
+hs1 = House("Горизонт", 15)
+hs2 = House("Гуливер", 15)
+
+print(hs1)
+print(hs2)
+
+print(hs1 == hs2)
+
+hs1 = hs1 + 10
+print(hs1)
+print(hs1 == hs2)
+
+hs2 += 5
+print(hs2)
+
+hs2 = 10 + hs2
+print(hs2)
+
+print(hs1 > hs2)
+print(hs1 >= hs2)
+print(hs1 < hs2) 
+print(hs1 <= hs2) 
+print(hs1 != hs2) 
