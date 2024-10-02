0ac206f45ab841cce4e3f9df7ad8586505fb610c
Сергей <nikitos841@yandex.ru>
2024-10-02 20:15:10 +0400

    corrected code

--- module_5_3.py
+++ module_5_3.py
@@ -3,15 +3,13 @@ class House:
     def __init__(self, name: str, number_of_floors: int):
         self.name = name
         self.number_of_floors = number_of_floors
-        self.valid(number_of_floors)
+        self.verif(number_of_floors)
 
-    def valid(self, values: int):
+    def verif(self, values: int):
         if not (
             isinstance(self.name, str) and
             isinstance(self.number_of_floors, int) and
-            isinstance(values, int) or
-            isinstance(values, House)
-
+            isinstance(values, (int, House))
         ):
             raise TypeError("Неверный фоомат(type) введённых данных")
 
@@ -22,44 +20,44 @@ class House:
         return self.number_of_floors
     
     def __add__(self, other):
-        self.valid(other)
+        self.verif(other)
         self.number_of_floors += other
         return self
     
     def __radd__(self, other):
-        self.valid(other)
+        self.verif(other)
         return self.__add__(other)
     
     def __iadd__(self, other):
-        self.valid(other)
+        self.verif(other)
         return self.__add__(other)
     
     def __eq__(self, other):
-        self.valid(other)
+        self.verif(other)
         return self.number_of_floors == other
     
-    def __ne__(self, other):
-        self.valid(other)
-        return self.number_of_floors != other
+    # def __ne__(self, other):
+    #     self.verif(other)
+    #     return not self.__eq__(other) # Это помоему так и работает
     
     def __gt__(self, other):
-        self.valid(other)
+        self.verif(other)
         return self.number_of_floors > other
     
     def __ge__(self, other):
-        self.valid(other)
+        self.verif(other)
         return self.number_of_floors >= other
     
     def __lt__(self, other):
-        self.valid(other)
+        self.verif(other)
         return self.number_of_floors < other
     
     def __le__(self, other):
-        self.valid(other)
+        self.verif(other)
         return self.number_of_floors <= other
 
     def go_to(self, new_floor: int):
-        self.valid(new_floor)
+        self.verif(new_floor)
         if 1 <= new_floor <= self.number_of_floors:
             for floor in range(1, new_floor+1):
                 print(floor)
