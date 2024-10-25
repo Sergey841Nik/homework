e42d5d01a8eb7b23ef3867861d012b4c9dc5b20d
Сергей <nikitos841@yandex.ru>
2024-10-25 19:58:36 +0400

    error_correction_module_7_dop

--- module_7_dop.py
+++ module_7_dop.py
@@ -3,14 +3,15 @@ class Context:
         self.__values: tuple = ()
 
     def __enter__(self) -> list:
-        self.__values = list(self.__values)
-        return self.__values
+        self.__values_copy = list(self.__values)
+        return self.__values_copy
     
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> tuple:
         if exc_type is None: #если не было исключений
-            self.__values = tuple(self.__values)
+            self.__values = tuple(self.__values_copy)
+            return self.__values
+        return self.__values
         
-
     def __str__(self) -> str:
         return str(self.__values)
     
@@ -19,6 +20,19 @@ print(cx)
 
 with cx as x:
     x.append(2)
-    x.append(4)
+
+print(cx)
+
+try:
+    with cx as x:
+        x.append(2)
+        kjkj
+except Exception:
+    pass
+
+print(cx)
+
+with cx as x:
+    x.append(22)
 
 print(cx)
