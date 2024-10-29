48350246e6373414f426db77abc7e70e04acfb30
Сергей <nikitos841@yandex.ru>
2024-10-29 19:22:40 +0400

    changed_it_according_to_the inspector_comments

--- module_8_3.py
+++ module_8_3.py
@@ -4,31 +4,32 @@ class IncorrectVinNumber(Exception):
     """
     Класс исключений для Vin номера автомобиля
     """
-    def __init__(self, *args) -> None:
-        self.message = args[0] if args else None
+    def __init__(self, message: str) -> None:
+        self.message = message if message else None
 
     def __str__(self) -> str:
-        return f"Неверный диапазон для Vin номера {self.message}"
+        return self.message
     
 class IncorrectCarNumbers(Exception):
     """
     Класс исключений для госномера автомобиля
     """
-    def __init__(self, *args) -> None:
-        self.message = args[0] if args else None
+    def __init__(self, message: str) -> None:
+        self.message = message if message else None
 
     def __str__(self) -> str:
-        return f"Госномер {self.message} не соответствует принятому в РФ шаблону"
+        return self.message
     
+#проверку на тип передаваемых данных вынес в отдельный класс
 class IncorrectTypesInfjCar(Exception):
     """
     Класс исключений для типа передаваемых данных
     """
-    def __init__(self, *args) -> None:
-        self.message = args[0] if args else None
+    def __init__(self, message: str) -> None:
+        self.message = message if message else None
 
     def __str__(self) -> str:
-        return f"Неверный тип {self.message}"
+        return self.message
     
 class Car:
     def __init__(self, model: str, vin_number: int, car_number: str) -> None:
@@ -39,23 +40,24 @@ class Car:
         self.__is_valid_vin(self.__vin)
         self.__is_valid_numbers(self.__numbers)
 
+
     def __is_valid_type(self, *args)-> None:
         if not isinstance(args[0], str):
-            raise IncorrectTypesInfjCar('модели')
+            raise IncorrectTypesInfjCar('Некорректный тип названия модели')
         elif not isinstance(args[1], int):
-            raise IncorrectTypesInfjCar('vin номера')
+            raise IncorrectTypesInfjCar('Некорректный тип vin номер')
         elif not isinstance(args[2], str):
-            raise IncorrectTypesInfjCar('госномера')
+            raise IncorrectTypesInfjCar('Некорректный тип данных для номеров',)
         
     def __is_valid_vin(self, vin_number)-> None:
         if not 1_000_000 <= vin_number <= 9_999_999:
-            raise IncorrectVinNumber(vin_number)
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
         
     def __is_valid_numbers(self, numbers)-> None:
         #создаём шаблон для госномера, без учёта регистра
         pattern = r'^[а-яА-Я]\d{3}[а-яА-Я]{2}$'
         if not re.match(pattern, numbers): #проверяем переданный номер на соответствие шаблону, в том числе проверяет длину строки
-            raise IncorrectCarNumbers(numbers)
+            raise IncorrectCarNumbers('Неверная длина номера или формата принятого в РФ')
         
     def __str__(self) -> str:
         return f" Автомобиль - {self.model}, VIN - {self.__vin}, госномер - {self.__numbers}"
