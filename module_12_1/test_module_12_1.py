ff11aed6086d203b9a594d91d535943935aaad01
Сергей <nikitos841@yandex.ru>
2024-12-10 20:17:42 +0400

    add module 12_1

--- module_12_1/test_module_12_1.py
+++ module_12_1/test_module_12_1.py
@@ -1,18 +1,5 @@
 from unittest import TestCase, main
-
-class Runner:
-    def __init__(self, name):
-        self.name = name
-        self.distance = 0
-
-    def run(self):
-        self.distance += 10
-
-    def walk(self):
-        self.distance += 5
-
-    def __str__(self):
-        return self.name
+from module_12 import Runner
 
 class RunnerTest(TestCase):
     def test_walk(self):
