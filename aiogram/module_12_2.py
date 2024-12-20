b816ffc7e14f836ecd9d4338f4b3999c141bb778
Сергей <nikitos841@yandex.ru>
2024-12-20 22:30:12 +0400

    add_module 12_2

---
+++ aiogram/module_12_2.py
@@ -0,0 +1,39 @@
+
+import os
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv()) #найти и загрузить переменные окружения из файла .env
+
+TOKEN = os.getenv('TOKEN') #получаем токен из переменного окружения
+
+bot = Bot(token=TOKEN)
+
+#экземпляр класса Bot сюда передаётся через start_polling
+#по умолчанию storage=MemoryStorage()
+#поэтому не передаём в Dispatcher() ни каких данных, как в видео уроке
+dp = Dispatcher() 
+
+@dp.message(CommandStart())
+async def start(message: types.Message) -> None:
+    print('Привет! Я бот помогающий твоему здоровью.')
+    #ответ от бота в ТГ на команду /start
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')   
+
+@dp.message()
+async def all_massages(message: types.Message) -> None:
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    #дразнит (отвечает эхом)
+    await message.answer(message.text)
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+asyncio.run(main())
+
+
