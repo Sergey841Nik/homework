52a3dc2164f8e11a7833e05747f9ce9ea93766ad
Сергей <nikitos841@yandex.ru>
2024-12-24 21:11:20 +0400

    module_13_5 corrected as directed by the teacher

--- aiogram/module_13_5.py
+++ aiogram/module_13_5.py
@@ -1,9 +1,9 @@
 import os
 import asyncio
 
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, StateFilter
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 
@@ -30,7 +30,7 @@ start_kb = ReplyKeyboardMarkup(
 
 
 @dp.message(CommandStart())
-async def start(message: types.Message) -> None:
+async def start(message: Message) -> None:
     await message.answer(
         "Привет! Я бот помогающий твоему здоровью", reply_markup=start_kb
     )
@@ -45,7 +45,7 @@ class UserState(StatesGroup):
 
 
 @dp.message(StateFilter(None), F.text == "Рассчитать")
-async def set_age(message: types.Message, state: FSMContext):
+async def set_age(message: Message, state: FSMContext):
     await message.answer("Введите свой возраст:")
     await state.set_state(UserState.age)
 
@@ -58,21 +58,21 @@ async def set_growth(message: types.Message, state: FSMContext):
 
 
 @dp.message(UserState.growth)
-async def set_weight(message: types.Message, state: FSMContext):
+async def set_weight(message: Message, state: FSMContext):
     await state.update_data(growth=message.text)
     await message.answer("Введите свой вес:")
     await state.set_state(UserState.weight)
 
 
 @dp.message(UserState.weight)
-async def set_sex(message: types.Message, state: FSMContext):
+async def set_sex(message: Message, state: FSMContext):
     await state.update_data(weight=message.text)
     await message.answer("Введите Ваш пол в формате М или Ж:")
     await state.set_state(UserState.sex)
 
 
 @dp.message(UserState.sex)
-async def send_calories(message: types.Message, state: FSMContext):
+async def send_calories(message: Message, state: FSMContext):
     await state.update_data(sex=message.text.lower())
     data = await state.get_data()
     if data["sex"] == "м":
@@ -97,6 +97,11 @@ async def send_calories(message: types.Message, state: FSMContext):
         )
 
     await state.clear()
+
+@dp.message()
+async def all_massages(message: Message) -> None:
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
 
 
 async def main() -> None:
